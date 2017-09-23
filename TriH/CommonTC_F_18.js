3a4ae3c5a18f4f4b0c64a35c02062c891c9a07a0
NguyenVanLeNguyen <nguyenphihoangbg@gmail.com>
2017-09-23 22:05:33 +0700

    fix-hard-code

--- TriH/CommonTC_F_18.js
+++ TriH/CommonTC_F_18.js
@@ -1,15 +1,15 @@
 describe('CommonTC_F_16', function() {
-	it('Test',function(){
+	it('CommonTC_F_16 hien thong bao xac nhan xoa',function(){
 
 		cy
 
-			.visit("http://52.187.8.102/signin")
+			.visit(Cypress.env('sign'))
 
 			.get("input[name='email']").clear().type("admin@gmail.com")
 
 			.get("input[name='password']").type("Methadone@2017").type("{enter}")
 
-			.visit("http://52.187.8.102/main/admin/administrators/issuing_agency")
+			.visit(Cypress.env('issuing_agency'))
 
 			.get("td[class='ng-binding']").contains("10118").parent().within(function(){
 
--- TriH/CommonTC_F_20.js
+++ TriH/CommonTC_F_20.js
@@ -3,13 +3,13 @@ describe('CommonTC_F_20', function() {
 
 		cy
 
-			.visit("http://52.187.8.102/signin")
+			.visit(Cypress.env('sign'))
 
 			.get("input[name='email']").clear().type("admin@gmail.com")
 
 			.get("input[name='password']").type("Methadone@2017").type("{enter}")
 
-			.visit("http://52.187.8.102/main/admin/administrators")
+			.visit(Cypress.env('administrators'))
 
 			.get("td[class='ng-binding']").contains("1").parent().within(function(){
 
