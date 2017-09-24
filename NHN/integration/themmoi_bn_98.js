4bffd0670a4895f0f8176767339aecc5663f7331
Viet Hoa Nguyen <viethoanguyen97@gmail.com>
2017-09-25 01:53:29 +0700

    NHN

---
+++ NHN/integration/themmoi_bn_98.js
@@ -0,0 +1,24 @@
+const delay = 2000
+describe('My First Test', function(){
+	describe('Đăng nhập ', function() {
+		it('Đăng nhập ', function(){
+		    cy.visit('http://13.76.80.144/signin')
+		    cy.get('input[name=email]').type('doctor_10@gmail.com')
+		    cy.get('input[name=password]').type('Methadone@2017')
+				cy.get('button[type=submit]').click()
+		  })
+	})
+
+	describe('Thêm mới bệnh nhân ', function(){
+		it('Chuyển đến trang thêm mới bệnh nhân', function () {
+        cy.wait(delay)
+      	cy.visit('http://13.76.80.144/main/patients/new')
+    })
+
+		it('Kiểm tra số lượng và sắp xếp các giá trị trong combo', function(){
+			cy.get('.ui-select-container[ng-model="patient.resident_district_id"]').text()
+		})
+
+
+	})
+})
