e33c12c5ba00c15f101db23970e75eea4cc3b318
bqcuong <bqcuong2212@gmail.com>
2017-09-24 21:38:54 +0700

    finish

--- DevNightmare/cypress/integration/add_patient_tc_spec.js
+++ DevNightmare/cypress/integration/add_patient_tc_spec.js
@@ -1,70 +1,74 @@
 describe('Them moi benh nhan', function() {
-  context('Kiem tra truong "Ngay vao dieu tri"', function() {
+  beforeEach(function() {
+    cy.visit('/signin');
 
-    beforeEach(function() {
-      cy.visit('/signin');
-
-      cy.fixture('users').then(users => {
-        cy.doLoginAs(users.doctor);
-      })
-
-      cy.visit('/main/patients/new')
+    cy.fixture('users').then(users => {
+      cy.doLoginAs(users.doctor);
     })
 
-    const completeRequiredInformation = function() {
-      cy.fixture('sample_patient').then(patient => {
+    cy.visit('/main/patients/new')
+  })
 
-        // ho va ten
-        cy.get('input[ng-model="patient.name"]').type(patient.name)
+  const completeRequiredInformation = function() {
+    cy.fixture('sample_patient').then(patient => {
 
-        // ngay sinh
-        cy.get('input[ng-model="patient.birthdate"]').type(patient.birth_date)
+      // ho va ten
+      cy.get('input[ng-model="patient.name"]').type(patient.name)
 
-        // gioi tinh
-        cy.doSelect2('div[ng-model="patient.gender"]', patient.gender)
+      // ngay sinh
+      cy.get('input[ng-model="patient.birthdate"]').type(patient.birth_date)
 
-        // tinh thuong tru
-        cy.doSelect2('div[ng-model="patient.province_id"]', patient.province_id)
+      // gioi tinh
+      cy.doSelect2('div[ng-model="patient.gender"]', patient.gender)
 
-        // huyen thuong tru
-        cy.doSelect2('div[ng-model="patient.district_id"]', patient.district_id)
+      // tinh thuong tru
+      cy.doSelect2('div[ng-model="patient.province_id"]', patient.province_id)
 
-        // xa thuong tru
-        cy.doSelect2('div[ng-model="patient.ward_id"]', patient.ward_id)
+      // huyen thuong tru
+      cy.doSelect2('div[ng-model="patient.district_id"]', patient.district_id)
 
-        // tinh tam tru
-        cy.doSelect2('div[ng-model="patient.resident_province_id"]', patient.resident_province_id)
+      // xa thuong tru
+      cy.doSelect2('div[ng-model="patient.ward_id"]', patient.ward_id)
 
-        // huyen tam tru
-        cy.doSelect2('div[ng-model="patient.resident_district_id"]', patient.resident_district_id)
+      // tinh tam tru
+      cy.doSelect2('div[ng-model="patient.resident_province_id"]', patient.resident_province_id)
 
-        // xa tam tru
-        cy.doSelect2('div[ng-model="patient.resident_ward_id"]', patient.resident_ward_id)
+      // huyen tam tru
+      cy.doSelect2('div[ng-model="patient.resident_district_id"]', patient.resident_district_id)
 
-        // dien cmnd
-        cy.doSelect2('div[ng-model="patient.identification_type"]', patient.identification_type)
-        cy.get('input[ng-model="patient.identification_number"]').type(patient.identification_number)
-        cy.get('input[ng-model="patient.identification_issued_date"]').type(patient.identification_issued_date)
-        cy.get('input[ng-model="patient.identification_issued_by"]').type(patient.identification_issued_by)
+      // xa tam tru
+      cy.doSelect2('div[ng-model="patient.resident_ward_id"]', patient.resident_ward_id)
 
-        // nguoi than
-        cy.get('input[ng-model="contact.name"]').type(patient.contacts[0].name)
-        cy.doSelect2('div[ng-model="contact.contact_type"]', patient.contacts[0].contact_type)
-      })
-    }
+      // dien cmnd
+      cy.doSelect2('div[ng-model="patient.identification_type"]', patient.identification_type)
+      cy.get('input[ng-model="patient.identification_number"]').type(patient.identification_number)
+      cy.get('input[ng-model="patient.identification_issued_date"]').type(patient.identification_issued_date)
+      cy.get('input[ng-model="patient.identification_issued_by"]').type(patient.identification_issued_by)
 
-    const getPatientInfoPortlet = function() {
-      return cy.get('.portlet').eq(1)
-    }
+      // nguoi than
+      cy.get('input[ng-model="contact.name"]').type(patient.contacts[0].name)
+      cy.doSelect2('div[ng-model="contact.contact_type"]', patient.contacts[0].contact_type)
+    })
+  }
 
-    const getAdmissionDateInput = function() {
-      return cy.get('input[ng-model="patient.admission_date"]') 
-    }
+  const getPatientInfoPortlet = function() {
+    return cy.get('.portlet').eq(1)
+  }
 
-    const submitForm = function() {
-      cy.get('button[type="submit"]').click()
-    }
-    
+  const getPatientListPortlet = function() {
+    return cy.get('.portlet').eq(0)
+  }
+
+  const submitForm = function() {
+    cy.get('button[type="submit"]').click()
+  }
+
+  const getAdmissionDateInput = function() {
+    return cy.get('input[ng-model="patient.admission_date"]') 
+  }
+  
+  context('Kiem tra truong "Ngay vao dieu tri"', function() {
+  
     const addDays = function(date, days) {
       var result = new Date(date)
       result.setDate(date.getDate() + days)
@@ -76,7 +80,6 @@ describe('Them moi benh nhan', function() {
              + ('0' + (date.getMonth()+1)).slice(-2) + '/'
              + date.getFullYear();
     }
-
 
     it('THEMMOI_BN_50', function() {
       getPatientInfoPortlet().within(function() {
@@ -140,4 +143,30 @@ describe('Them moi benh nhan', function() {
     })
   })
 
+  context('Chuc nang', function() {
+
+    it('THEMMOI_BN_165', function() {
+      getPatientInfoPortlet().within(function() {
+        completeRequiredInformation()
+        cy.fixture('sample_patient').then(
+          patient => getAdmissionDateInput().type(patient.admission_date))
+        submitForm()
+      })
+      
+      // kiem tra thong bao them moi thanh cong
+      cy.contains('Tạo mới thành công').should('be.visible')
+
+      // kiem tra xem ban ghi duoc hien thi dau tien trong danh sach
+      cy.visit('/main/patients')
+      getPatientListPortlet().within(function() {
+        cy.fixture('sample_patient').then(
+          patient => cy.get('table > tbody > tr').first().should('contain', patient.name))
+      })
+
+    })
+ 
+    it('THEMMOI_BN_166', function() {
+      // always true
+    })
+  })
 })
