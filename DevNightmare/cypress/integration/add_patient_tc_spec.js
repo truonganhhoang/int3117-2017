d32693a7c3f35d8da42eb9d16fcd6526b96dca26
bqcuong <bqcuong2212@gmail.com>
2017-09-24 17:08:58 +0700

    done beforeEach for add patient

--- DevNightmare/cypress/integration/add_patient_tc_spec.js
+++ DevNightmare/cypress/integration/add_patient_tc_spec.js
@@ -12,9 +12,43 @@ describe('Them moi benh nhan', function() {
 
       cy.fixture('sample_patient').then(patient => {
         cy.get('.portlet').last().within(function() {
+
+          // ho va ten
           cy.get('input[ng-model="patient.name"]').type(patient.name)
+
+          // ngay sinh
           cy.get('input[ng-model="patient.birthdate"]').type(patient.birth_date)
-//          cy.get('div[ng-model="patient.gender"]').select(patient.gender)
+
+          // gioi tinh
+          cy.doSelect2('div[ng-model="patient.gender"]', patient.gender)
+
+          // tinh thuong tru
+          cy.doSelect2('div[ng-model="patient.province_id"]', patient.province_id)
+
+          // huyen thuong tru
+          cy.doSelect2('div[ng-model="patient.district_id"]', patient.district_id)
+
+          // xa thuong tru
+          cy.doSelect2('div[ng-model="patient.ward_id"]', patient.ward_id)
+
+          // tinh tam tru
+          cy.doSelect2('div[ng-model="patient.resident_province_id"]', patient.resident_province_id)
+
+          // huyen tam tru
+          cy.doSelect2('div[ng-model="patient.resident_district_id"]', patient.resident_district_id)
+
+          // xa tam tru
+          cy.doSelect2('div[ng-model="patient.resident_ward_id"]', patient.resident_ward_id)
+
+          // dien cmnd
+          cy.doSelect2('div[ng-model="patient.identification_type"]', patient.identification_type)
+          cy.get('input[ng-model="patient.identification_number"]').type(patient.identification_number)
+          cy.get('input[ng-model="patient.identification_issued_date"]').type(patient.identification_issued_date)
+          cy.get('input[ng-model="patient.identification_issued_by"]').type(patient.identification_issued_by)
+
+          // nguoi than
+          cy.get('input[ng-model="contact.name"]').type(patient.contacts[0].name)
+          cy.doSelect2('div[ng-model="contact.contact_type"]', patient.contacts[0].contact_type)
 
         })
       })
