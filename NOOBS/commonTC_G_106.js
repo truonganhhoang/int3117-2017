a48f49a4752a19ef6d325b91f3e9e62d830ce079
huuchi207 <huuchi207@gmail.com>
2017-09-24 16:53:04 +0700

    separate function

--- NOOBS/commonTC_G_106.js
+++ NOOBS/commonTC_G_106.js
@@ -1,29 +1,53 @@
-let delay =3000; // 3second delay trang mang lag
+let delay =3000; // 3 seconds delay
 describe('Upload file anh sai duong dan', function () {
-    describe('Dang nhap bang tai khoan admin_agency_10@gmail.com', () => {
+    describe('Login with account: admin_agency_10@gmail.com', function() {
         it('Login ...', () => {
-            cy.visit(Cypress.env("host"))
-            cy.get('input[name=email]').type('admin_agency_10@gmail.com')
-            cy.get('input[name=password]').type('Methadone@2017')
-            cy.get('button[type=submit]').click()
-        })
+            cy.visit(Cypress.env("URL_LOGIN"));
+        cy.get('input[name=email]').type(Cypress.env("USER_AGENCY");
+        cy.get('input[name=password]').type(Cypress.env("LOGIN_PASSWORD"));
+        cy.get('button[type=submit]').click();
+    })
     })
 
-    describe('Vao module Quan Ly Benh Nhan', () => {
-        it('Chinh sua duong dan anh', () => {
-            cy.wait(delay);
-            cy.visit(Cypress.env("edit_info_patient_504"));
-            cy.wait(delay);
-            cy.get('.img-thumbnail').then(function($img){
-                // append the image
-                $img.attr('src', 'sdfsfsdfs');
-            });
-            cy.get('button[type=submit]').first().click();
-            cy.get('div[ng-switch]')
-                .find('.toast-message')
-                .should('contain', "Cập nhật thông tin thành công");
+    describe('Patient Management Module', function() {
+        describe('Add New Patient', function () {
+            it('Go to Module', function () {
+                goToModule();
+            } )
+            it('Enter right info', function () {
+                enterRightInfo();
+            })
+            it('Edit avatar directory', function () {
+                editAvatarDirectory('sdfklsjlf');
+                // check result
+                cy.get('div[ng-switch]')
+                    .find('.toast-message')
+                    .should('contain', "Cập nhật thông tin thành công");
+            })
         })
     })
-
+    function enterRightInfo() {
+        // cy.get('input[name=name]').first().type('Nguyen Ba Huu Chi');
+        // cy.get('input[name=birth_date]').type('20/07/1996');
+        // cy.get('.select2-chosen').first().click();
+        // cy.wait(delay);
+        // cy.get('.select2-highlighted').first().click();
+        // cy.get('input[name=mobile_phone]').type('0123456789');
+        // cy.get('input[name=admission_date]').type('16/08/2017');
+        cy.get('div[name=ward_id]').first().click();
+        Cypress.$('#ui-select-choices-row-48-2 > div').click();
+    }
+    function goToModule() {
+        cy.wait(delay);
+        cy.visit(Cypress.env("URL_ADD_NEW_PATIENT"));
+        cy.wait(delay);
+    }
+    function editAvatarDirectory(newDir) {
+        cy.get('.img-thumbnail').then(function($img){
+            // edit the image dir
+            $img.attr('src', newDir);
+        });
+        cy.get('button[type=submit]').first().click();
+    }
 })
 
