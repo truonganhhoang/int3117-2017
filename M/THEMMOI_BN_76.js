e7972a905ba542cb6c3b28741bea217fdbad1a88
khuyen97 <khanhhuyentb97@gmail.com>
2017-10-01 14:31:58 +0700

    them moi benh nhan

---
+++ M/THEMMOI_BN_76.js
@@ -0,0 +1,40 @@
+describe('Kiem tra chuc nang tim kiem tren combo-box',function(){
+    it('login',function(){
+        cy.visit('http://13.76.80.144/signin')
+        cy.get('input[name=email]').type('doctor_10@gmail.com')
+        cy.get('input[name=password]').type('Methadone@2017')
+        cy.get('button[type=submit]').click()
+        cy.pause()
+        cy.visit('http://13.76.80.144/main/patients/new')
+    })
+    it('Chon tinh/thanh pho thuong tru',function(){
+        cy
+        .get("label.required")
+        .contains("Tỉnh/Thành phố thường trú")
+        .parent()
+        .within(function(){
+          cy.root().get(".ui-select-container").click()  
+        })
+        cy.get('#ui-select-choices-row-19-22').click()
+    })
+    it('Chon huyen/quan thuong tru',function(){
+        cy
+        .get("label.required")
+        .contains("Huyện/Quận thường trú")
+        .parent()
+        .within(function(){
+          cy.root().get(".ui-select-container").click()  
+        })
+        cy.get('#ui-select-choices-row-20-4').click()
+    })
+    it('Chon xa/thi tran thuong tru',function(){
+        cy
+        .get("label.required")
+        .contains("Xã/Thị Trấn thường trú")
+        .parent()
+        .within(function(){
+          cy.get(".ui-select-container").click()
+          cy.get("input[type=search]").first().type("Đại ").focus().click()
+        })
+    })
+})
