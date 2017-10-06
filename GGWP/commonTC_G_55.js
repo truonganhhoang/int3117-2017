b6d506b2ec1d39fe6ea75d567cb70f1884ab0f05
haileebn <hainvyp@gmail.com>
2017-10-06 22:33:50 +0700

    final

--- GGWP/commonTC_G_55.js
+++ GGWP/commonTC_G_55.js
@@ -1,5 +1,5 @@
 /**
- * fail
+ * pass
  * TC Combo Box
  * 
  * co Combo Box
@@ -29,104 +29,112 @@ describe(`Kiem tra Combo Box voi tai khoan ${mail}`, function () {
     })
 
     describe('Danh mục nghề nghiệp', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/employments')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
 
     describe('Danh mục hôn nhân', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/maritals')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục tài chính', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/financials')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục trình độ học vấn', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/educations')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục lý do ngừng điều trị', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/stop_reasons')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục thuốc', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/medicine_list')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục nhà sản xuất ', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/manufacturers')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
     describe('Danh mục nhà phân phối', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/providers')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     }) 
     describe('Danh mục nguồn thuốc', function () {
-        it('Gia tri mac dinh nen la ""', function () {
+        it('Gia tri duoc can le trai', function () {
             // cy.wait(delay)
             cy.visit('main/admin/administrators/sources')
             cy.wait(delay)
             cy.get("label").contains("Trạng thái:")
-            .next().find('a.select2-choice')
-            .should('have.attr', 'placeholder', '')
-            // .should('have.attr', 'placeholder', '-- Tất cả---')
+            .next().find('a.select2-choice').click()
+            cy.get('#ui-select-choices-0')
+                .and('have.css', 'text-align')
+                .and('match', /left/)
         })
     })
-
 })
