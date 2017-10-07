9da5cbdf6bd979593e606e3128d1e4a25cb1d838
DzungBui <dungbt0108@gmail.com>
2017-10-07 11:38:18 +0700

    update themmoi94

--- MID/cypress/integration/THEMMOI_BN_94.js
+++ MID/cypress/integration/THEMMOI_BN_94.js
@@ -1,28 +1,23 @@
 var delay = 1000;
 describe('Kiểm tra combo-box "Tỉnh/Thành phố tạm trú"', function () {
-    describe('Tài khoản quản trị hệ thống', function () {
-        it('Đăng nhập...', function () {
-            cy.fixture('users').then((user) =>{
-      			cy.visit('/signin');
-      			cy.get('input[name=email]').type(user.doctor.email);
-      			cy.get('input[name=password]').type(user.doctor.password);
-      			cy.get('button[type=submit]').click();
-      			cy.wait(delay);
-  			})
+  it('Đăng nhập...', function () {
+    cy.fixture('users').then((user) =>{
+      cy.visit('/signin');
+      cy.get('input[name=email]').type(user.doctor.email);
+      cy.get('input[name=password]').type(user.doctor.password);
+      cy.get('button[type=submit]').click();
+      cy.wait(delay);
   		})
-    });
+  	})
 
-    describe('Kiểm tra căn lề trái', function () {
-    it('Thêm bệnh nhân...', function () {
-                cy.get('li.nav-item > a[ui-sref="main.patients"]').click().wait(1000);
-                cy.get('a.btn[href="/main/patients/new"]').click().wait(1000);
-                cy.contains('div.portlet', 'Thông tin chi tiết').should('visible');
-    });
-    it('Test', function() {
-        var ele = cy.get("label").contains("Tỉnh/Thành phố tạm trú").next();
-        ele.click();
-
-        cy.get('li[role="option"] > div > span').should('have.css', 'text-align', 'left');
-        });
-    });
-});
+  it('Thêm bệnh nhân...', function () {
+    cy.get('li.nav-item > a[ui-sref="main.patients"]').click().wait(1000);
+    cy.get('a.btn[href="/main/patients/new"]').click().wait(1000);
+    cy.contains('div.portlet', 'Thông tin chi tiết').should('visible');
+  })
+  it('Test', function() {
+    var ele = cy.get("label").contains("Tỉnh/Thành phố tạm trú").next().next();
+    ele.click();
+    cy.get('li[role="option"] > div > span').should('have.css', 'text-align', 'left');
+  })
+})
